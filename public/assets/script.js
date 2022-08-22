b856cdcc6f91715cafd66b962383dda6c2f26224
IvansMuzicenko <ivans.muzicenko@gmail.com>
2022-08-22 15:45:36 +0300

    feat: assets links

---
+++ public/assets/script.js
@@ -0,0 +1,6 @@
+import "./scripts/auth.css";
+import "./scripts/catalogue-item.css";
+import "./scripts/catalogue.css";
+import "./scripts/index.css";
+import "./scripts/profile.css";
+import "./scripts/cart.css";
