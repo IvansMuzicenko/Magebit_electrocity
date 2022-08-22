1798c112ba068a150c9f8d9a14dbf90ee4106e67
Alex-Comrade <zabeckisa@gmail.com>
2022-08-22 15:47:26 +0300

    feat: Added CSS to auth page + .js file

--- resources/views/auth.blade.php
+++ resources/views/auth.blade.php
@@ -2,9 +2,10 @@
 require_once "./templates/header.php"
 ?>
 
+<!--SIGN IN-->
 
-<div class="container d-flex justify-content-around">
-  <div class="signIn">
+<div class="container d-flex justify-content-around mt-5">
+  <div class="signIn bg-secondary p-3 visually-hidden">
     <h2 class="d-flex justify-content-center mb-5">User Login</h2>
     <div class="form-floating mb-3">
       <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com" />
@@ -17,9 +18,12 @@ require_once "./templates/header.php"
     <div class="d-flex justify-content-center mb-3">
       <button type="submit" class="btn btn-primary btn-lg">Log-in</button>
     </div>
+    <p>Don't have an account? <a href="">Sign-up!</a></p>
   </div>
 
-  <form class="signUp ">
+  <!--SIGN UP-->
+
+  <form class="signUp bg-secondary p-3 ">
     <h2 class="d-flex justify-content-center mb-5">User registration</h2>
     <div class="reg-container-main d-flex gap-3">
       <div class="reg-container-1">
@@ -44,16 +48,17 @@ require_once "./templates/header.php"
           <label for="floatingPassword">Repeat password</label>
         </div>
       </div>
-      <div class="reg-container-2">
+      <div class="reg-container-2 ">
         <div class="form-floating">
           <input class="form-control" placeholder="Street, city, zip-code" id="floatingTextarea2" style="height: 250px"></input>
           <label for="floatingTextarea2">Street, city, zip-code</label>
         </div>
       </div>
     </div>
-    <div class="d-flex justify-content-center mb-3">
+    <div class="d-flex justify-content-center mb-5 mt-5">
       <button type="submit" class="btn btn-primary btn-lg">Register</button>
     </div>
+    <p class="d-flex justify-content-center mb-3 ">Already have an account? <a href="">Log-in</a></p>
   </form>
 </div>
 
