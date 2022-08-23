3a78727fcd8b774c0f82f14c9cf6b234991ab3d0
IvansMuzicenko <ivans.muzicenko@gmail.com>
2022-08-23 14:30:58 +0300

    feat: 404 page game

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -2,119 +2,319 @@
 require_once "./templates/header.php"
 ?>
 
-    <div class="page">
-      <div class="game">
-        <h1 class="game__results"></h1>
-        <h1>Player <span class="game__turn">1</span> turn</h1>
-        <div class="game__area">
-          <div class="game__field"><p></p></div>
-          <div class="game__field"><p></p></div>
-          <div class="game__field"><p></p></div>
-          <div class="game__field"><p></p></div>
-          <div class="game__field"><p></p></div>
-          <div class="game__field"><p></p></div>
-          <div class="game__field"><p></p></div>
-          <div class="game__field"><p></p></div>
-          <div class="game__field"><p></p></div>
-          <div class="game__field"><p></p></div>
-          <div class="game__field"><p></p></div>
-          <div class="game__field"><p></p></div>
-          <div class="game__field"><p></p></div>
-          <div class="game__field"><p></p></div>
-          <div class="game__field"><p></p></div>
-          <div class="game__field"><p></p></div>
-          <div class="game__field"><p></p></div>
-          <div class="game__field"><p></p></div>
-          <div class="game__field"><p></p></div>
-          <div class="game__field"><p></p></div>
-          <div class="game__field"><p></p></div>
-          <div class="game__field"><p></p></div>
-          <div class="game__field"><p></p></div>
-          <div class="game__field"><p></p></div>
-          <div class="game__field"><p></p></div>
-          <div class="game__field"><p></p></div>
-          <div class="game__field"><p></p></div>
-          <div class="game__field"><p></p></div>
-          <div class="game__field"><p></p></div>
-          <div class="game__field"><p></p></div>
-          <div class="game__field"><p></p></div>
-          <div class="game__field"><p></p></div>
-          <div class="game__field"><p></p></div>
-          <div class="game__field"><p></p></div>
-          <div class="game__field"><p></p></div>
-          <div class="game__field"><p></p></div>
-          <div class="game__field"><p></p></div>
-          <div class="game__field"><p></p></div>
-          <div class="game__field"><p></p></div>
-          <div class="game__field"><p></p></div>
-          <div class="game__field"><p></p></div>
-          <div class="game__field"><p></p></div>
-          <div class="game__field"><p></p></div>
-          <div class="game__field"><p></p></div>
-          <div class="game__field"><p></p></div>
-          <div class="game__field"><p></p></div>
-          <div class="game__field"><p></p></div>
-          <div class="game__field"><p></p></div>
-          <div class="game__field"><p></p></div>
-          <div class="game__field"><p></p></div>
-          <div class="game__field"><p></p></div>
-          <div class="game__field"><p></p></div>
-          <div class="game__field"><p></p></div>
-          <div class="game__field"><p></p></div>
-          <div class="game__field"><p></p></div>
-          <div class="game__field"><p></p></div>
-          <div class="game__field"><p></p></div>
-          <div class="game__field"><p></p></div>
-          <div class="game__field"><p></p></div>
-          <div class="game__field"><p></p></div>
-          <div class="game__field"><p></p></div>
-          <div class="game__field"><p></p></div>
-          <div class="game__field"><p></p></div>
-          <div class="game__field"><p></p></div>
-          <div class="game__field"><p></p></div>
-          <div class="game__field"><p></p></div>
-          <div class="game__field"><p></p></div>
-          <div class="game__field"><p></p></div>
-          <div class="game__field"><p></p></div>
-          <div class="game__field"><p></p></div>
-          <div class="game__field"><p></p></div>
-          <div class="game__field"><p></p></div>
-          <div class="game__field"><p></p></div>
-          <div class="game__field"><p></p></div>
-          <div class="game__field"><p></p></div>
-          <div class="game__field"><p></p></div>
-          <div class="game__field"><p></p></div>
-          <div class="game__field"><p></p></div>
-          <div class="game__field"><p></p></div>
-          <div class="game__field"><p></p></div>
-          <div class="game__field"><p></p></div>
-          <div class="game__field"><p></p></div>
-          <div class="game__field"><p></p></div>
-          <div class="game__field"><p></p></div>
-          <div class="game__field"><p></p></div>
-          <div class="game__field"><p></p></div>
-          <div class="game__field"><p></p></div>
-          <div class="game__field"><p></p></div>
-          <div class="game__field"><p></p></div>
-          <div class="game__field"><p></p></div>
-          <div class="game__field"><p></p></div>
-          <div class="game__field"><p></p></div>
-          <div class="game__field"><p></p></div>
-          <div class="game__field"><p></p></div>
-          <div class="game__field"><p></p></div>
-          <div class="game__field"><p></p></div>
-          <div class="game__field"><p></p></div>
-          <div class="game__field"><p></p></div>
-          <div class="game__field"><p></p></div>
-          <div class="game__field"><p></p></div>
-        </div>
-
-        <button class="game__reset" onclick="reset()" type="reset">
-          Reset
-        </button>
-        <p>Player 1 (X) | Player 2 (O)</p>
+<div class="page">
+  <div class="game">
+    <h1 class="game__results"></h1>
+    <h1>Player <span class="game__turn">1</span> turn</h1>
+    <div class="game__area">
+      <div class="game__field">
+        <p></p>
+      </div>
+      <div class="game__field">
+        <p></p>
+      </div>
+      <div class="game__field">
+        <p></p>
+      </div>
+      <div class="game__field">
+        <p></p>
+      </div>
+      <div class="game__field">
+        <p></p>
+      </div>
+      <div class="game__field">
+        <p></p>
+      </div>
+      <div class="game__field">
+        <p></p>
+      </div>
+      <div class="game__field">
+        <p></p>
+      </div>
+      <div class="game__field">
+        <p></p>
+      </div>
+      <div class="game__field">
+        <p></p>
+      </div>
+      <div class="game__field">
+        <p></p>
+      </div>
+      <div class="game__field">
+        <p></p>
+      </div>
+      <div class="game__field">
+        <p></p>
+      </div>
+      <div class="game__field">
+        <p></p>
+      </div>
+      <div class="game__field">
+        <p></p>
+      </div>
+      <div class="game__field">
+        <p></p>
+      </div>
+      <div class="game__field">
+        <p></p>
+      </div>
+      <div class="game__field">
+        <p></p>
+      </div>
+      <div class="game__field">
+        <p></p>
+      </div>
+      <div class="game__field">
+        <p></p>
+      </div>
+      <div class="game__field">
+        <p></p>
+      </div>
+      <div class="game__field">
+        <p></p>
+      </div>
+      <div class="game__field">
+        <p></p>
+      </div>
+      <div class="game__field">
+        <p></p>
+      </div>
+      <div class="game__field">
+        <p></p>
+      </div>
+      <div class="game__field">
+        <p></p>
+      </div>
+      <div class="game__field">
+        <p></p>
+      </div>
+      <div class="game__field">
+        <p></p>
+      </div>
+      <div class="game__field">
+        <p></p>
+      </div>
+      <div class="game__field">
+        <p></p>
+      </div>
+      <div class="game__field">
+        <p></p>
+      </div>
+      <div class="game__field">
+        <p></p>
+      </div>
+      <div class="game__field">
+        <p></p>
+      </div>
+      <div class="game__field">
+        <p></p>
+      </div>
+      <div class="game__field">
+        <p></p>
+      </div>
+      <div class="game__field">
+        <p></p>
+      </div>
+      <div class="game__field">
+        <p></p>
+      </div>
+      <div class="game__field">
+        <p></p>
+      </div>
+      <div class="game__field">
+        <p></p>
+      </div>
+      <div class="game__field">
+        <p></p>
+      </div>
+      <div class="game__field">
+        <p></p>
+      </div>
+      <div class="game__field">
+        <p></p>
+      </div>
+      <div class="game__field">
+        <p></p>
+      </div>
+      <div class="game__field">
+        <p></p>
+      </div>
+      <div class="game__field">
+        <p></p>
+      </div>
+      <div class="game__field">
+        <p></p>
+      </div>
+      <div class="game__field">
+        <p></p>
+      </div>
+      <div class="game__field">
+        <p></p>
+      </div>
+      <div class="game__field">
+        <p></p>
+      </div>
+      <div class="game__field">
+        <p></p>
+      </div>
+      <div class="game__field">
+        <p></p>
+      </div>
+      <div class="game__field">
+        <p></p>
+      </div>
+      <div class="game__field">
+        <p></p>
+      </div>
+      <div class="game__field">
+        <p></p>
+      </div>
+      <div class="game__field">
+        <p></p>
+      </div>
+      <div class="game__field">
+        <p></p>
+      </div>
+      <div class="game__field">
+        <p></p>
+      </div>
+      <div class="game__field">
+        <p></p>
+      </div>
+      <div class="game__field">
+        <p></p>
+      </div>
+      <div class="game__field">
+        <p></p>
+      </div>
+      <div class="game__field">
+        <p></p>
+      </div>
+      <div class="game__field">
+        <p></p>
+      </div>
+      <div class="game__field">
+        <p></p>
+      </div>
+      <div class="game__field">
+        <p></p>
+      </div>
+      <div class="game__field">
+        <p></p>
+      </div>
+      <div class="game__field">
+        <p></p>
+      </div>
+      <div class="game__field">
+        <p></p>
+      </div>
+      <div class="game__field">
+        <p></p>
+      </div>
+      <div class="game__field">
+        <p></p>
+      </div>
+      <div class="game__field">
+        <p></p>
+      </div>
+      <div class="game__field">
+        <p></p>
+      </div>
+      <div class="game__field">
+        <p></p>
+      </div>
+      <div class="game__field">
+        <p></p>
+      </div>
+      <div class="game__field">
+        <p></p>
+      </div>
+      <div class="game__field">
+        <p></p>
+      </div>
+      <div class="game__field">
+        <p></p>
+      </div>
+      <div class="game__field">
+        <p></p>
+      </div>
+      <div class="game__field">
+        <p></p>
+      </div>
+      <div class="game__field">
+        <p></p>
+      </div>
+      <div class="game__field">
+        <p></p>
+      </div>
+      <div class="game__field">
+        <p></p>
+      </div>
+      <div class="game__field">
+        <p></p>
+      </div>
+      <div class="game__field">
+        <p></p>
+      </div>
+      <div class="game__field">
+        <p></p>
+      </div>
+      <div class="game__field">
+        <p></p>
+      </div>
+      <div class="game__field">
+        <p></p>
+      </div>
+      <div class="game__field">
+        <p></p>
+      </div>
+      <div class="game__field">
+        <p></p>
+      </div>
+      <div class="game__field">
+        <p></p>
+      </div>
+      <div class="game__field">
+        <p></p>
+      </div>
+      <div class="game__field">
+        <p></p>
+      </div>
+      <div class="game__field">
+        <p></p>
+      </div>
+      <div class="game__field">
+        <p></p>
+      </div>
+      <div class="game__field">
+        <p></p>
+      </div>
+      <div class="game__field">
+        <p></p>
+      </div>
+      <div class="game__field">
+        <p></p>
+      </div>
+      <div class="game__field">
+        <p></p>
+      </div>
+      <div class="game__field">
+        <p></p>
+      </div>
+      <div class="game__field">
+        <p></p>
+      </div>
+      <div class="game__field">
+        <p></p>
       </div>
     </div>
+
+    <button class="game__reset">
+      Reset
+    </button>
+    <p>Player 1 (X) | Player 2 (O)</p>
+  </div>
+</div>
 
 
 <?php
