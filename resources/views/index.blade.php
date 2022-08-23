9ff13f4eaf73482c65756ad27258aaf128a0855c
Alex-Comrade <zabeckisa@gmail.com>
2022-08-23 11:08:38 +0300

    feat: index upgrade

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,69 +4,122 @@ require_once "./templates/header.php";
 
 
 <div class="bg-secondary">
-    <div class="overview row m-2">
-        <div class="col text-center">
-            <img height="100" src="../assets/images/logo.svg" alt="">
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium ab commodi soluta rerum vero ullam hic? Dicta dignissimos qui exercitationem id iure accusantium dolore cupiditate consequuntur voluptates eius neque non, aspernatur sapiente saepe quidem, assumenda delectus laboriosam nobis, hic quis provident nemo repellendus. Assumenda, nam dolorem deleniti inventore odio sint vitae consequatur vel aperiam nesciunt architecto incidunt quos, quia error exercitationem dolorum ab atque, officiis suscipit esse. Ipsa culpa, repudiandae quae quis accusamus nostrum commodi similique nemo a deserunt quaerat placeat unde autem in omnis excepturi quibusdam officia numquam dolor ad, rem vero ipsum? Magnam nesciunt aspernatur ab explicabo porro?</p>
-        </div>
-        <div class="col-5 mt-5">
-            <h3 class="text-center">Best seller</h3>
-            <div class="card mb-3 w-100">
-                <div class="row g-0">
-                    <div class="col-md-4">
-                        <img src="https://m.media-amazon.com/images/I/61BnR2NE4PL._AC_SX679_.jpg" class="img-fluid rounded-start" alt="...">
-                    </div>
-                    <div class="col-md-8">
-                        <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
-                            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                            <a class="btn" href="#">Show product</a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+  <div class="overview row m-2">
+    <div class="col text-center">
+      <img class="mb-3 pt-2" height="100" src="../assets/images/logo.svg" alt="">
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium ab commodi soluta rerum vero ullam hic? Dicta dignissimos qui exercitationem id iure accusantium dolore cupiditate consequuntur voluptates eius neque non, aspernatur sapiente saepe quidem, assumenda delectus laboriosam nobis, hic quis provident nemo repellendus. Assumenda, nam dolorem deleniti inventore odio sint vitae consequatur vel aperiam nesciunt architecto incidunt quos, quia error exercitationem dolorum ab atque, officiis suscipit esse. Ipsa culpa, repudiandae quae quis accusamus nostrum commodi similique nemo a deserunt quaerat placeat unde autem in omnis excepturi quibusdam officia numquam dolor ad, rem vero ipsum? Magnam nesciunt aspernatur ab explicabo porro?</p>
     </div>
-    <div class="catalogue row w-100 m-0">
-        <div class="card" style="width: 18rem;">
-            <img src="..." class="card-img-top" alt="...">
+    <div class="col-5 mt-5">
+      <h3 class="text-center">Best seller</h3>
+      <div class="card mb-3 w-100">
+        <div class="row g-0">
+          <div class="col-md-4">
+            <img src="https://m.media-amazon.com/images/I/61BnR2NE4PL._AC_SX679_.jpg" class="img-fluid rounded-start" alt="...">
+          </div>
+          <div class="col-md-8">
             <div class="card-body">
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+              <a class="btn" href="#">Show product</a>
             </div>
+          </div>
         </div>
-        <div class="card" style="width: 18rem;">
-            <img src="..." class="card-img-top" alt="...">
-            <div class="card-body">
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            </div>
-        </div>
-        <div class="card" style="width: 18rem;">
-            <img src="..." class="card-img-top" alt="...">
-            <div class="card-body">
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            </div>
-        </div>
-        <div class="card" style="width: 18rem;">
-            <img src="..." class="card-img-top" alt="...">
-            <div class="card-body">
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            </div>
-        </div>
-        <div class="card" style="width: 18rem;">
-            <img src="..." class="card-img-top" alt="...">
-            <div class="card-body">
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            </div>
-        </div>
-        <div class="card" style="width: 18rem;">
-            <img src="..." class="card-img-top" alt="...">
-            <div class="card-body">
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            </div>
-        </div>
+      </div>
+    </div>
+  </div>
 
+  <div class="d-flex justify-content-center mt-5">
+    <h3>Catalogue</h3>
+  </div>
+  <div class="catalogue row m-0 d-flex justify-content-center gap-2 mb-5 pb-2">
+
+    <a href="#" class="card" style="width: 18rem;">
+      <div>
+        <img src="..." class="card-img-top" alt="...">
+        <div class="card-body">
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        </div>
+      </div>
+    </a>
+    <a href="#" class="card" style="width: 18rem;">
+      <div>
+        <img src="..." class="card-img-top" alt="...">
+        <div class="card-body">
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        </div>
+      </div>
+    </a>
+    <a href="#" class="card" style="width: 18rem;">
+      <div>
+        <img src="..." class="card-img-top" alt="...">
+        <div class="card-body">
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        </div>
+      </div>
+    </a>
+    <a href="#" class="card" style="width: 18rem;">
+      <div>
+        <img src="..." class="card-img-top" alt="...">
+        <div class="card-body">
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        </div>
+      </div>
+    </a>
+    <a href="#" class="card" style="width: 18rem;">
+      <div>
+        <img src="..." class="card-img-top" alt="...">
+        <div class="card-body">
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        </div>
+      </div>
+    </a>
+    <a href="#" class="card" style="width: 18rem;">
+      <div>
+        <img src="..." class="card-img-top" alt="...">
+        <div class="card-body">
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        </div>
+      </div>
+    </a>
+    <a href="#" class="card" style="width: 18rem;">
+      <div>
+        <img src="..." class="card-img-top" alt="...">
+        <div class="card-body">
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        </div>
+      </div>
+    </a>
+    <a href="#" class="card" style="width: 18rem;">
+      <div>
+        <img src="..." class="card-img-top" alt="...">
+        <div class="card-body">
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        </div>
+      </div>
+    </a>
+    <a href="#" class="card" style="width: 18rem;">
+      <div>
+        <img src="..." class="card-img-top" alt="...">
+        <div class="card-body">
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        </div>
+      </div>
+    </a>
+    <a href="#" class="card" style="width: 18rem;">
+      <div>
+        <img src="..." class="card-img-top" alt="...">
+        <div class="card-body">
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        </div>
+      </div>
+    </a>
+    <div class="d-flex justify-content-center mt-5">
+      <a href="catalogue/" type="submit" class="btn btn-primary btn-lg mb-5 catalogue-btn">Catalogue</a>
     </div>
+
+  </div>
 </div>
+
 
 
 <?php
