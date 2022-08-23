53177c832fd40b0654eefa8bef7c431776b3d22c
IvansMuzicenko <ivans.muzicenko@gmail.com>
2022-08-23 12:44:30 +0300

    fix: auth

--- public/assets/script.js
+++ public/assets/script.js
@@ -1,5 +1,5 @@
-import "./scripts/404.js";
 import "./scripts/auth.js";
+import "./scripts/404.js";
 //import "./scripts/catalogue-item.js";
 //import "./scripts/catalogue.js";
 //import "./scripts/index.js";
