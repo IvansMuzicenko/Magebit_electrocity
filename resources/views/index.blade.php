3cc4dbde0e270d3f332be6bc56ae4ccd70f2b119
Dmitrijs <dmitrijs.sinickis@gmail.com>
2022-08-23 16:11:55 +0300

    fix banner

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,135 +2,139 @@
 require_once "./templates/header.php";
 ?>
 
-  <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
-  <div class="carousel-indicators">
-    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
-    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+<!-- BANNER -->
+
+<div id="carouselBanner" class="carousel slide" data-bs-ride="false">
+  <div id="carouselIndicator" class="carousel-indicators">
+    <button type="button" data-bs-target="#carouselBanner" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselBanner" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselBanner" data-bs-slide-to="2" aria-label="Slide 3"></button>
   </div>
   <div class="carousel-inner">
     <div class="carousel-item active">
       <img src="../assets/images/banner1.svg" class="d-block w-100" style="object-fit:cover" alt="...">
-      <div class="carousel-caption d-none d-md-block">
-        <h5>First slide label</h5>
-        <p>Some representative placeholder content for the first slide.</p>
+      <div id="textBg" class="carousel-caption d-none d-md-block">
+        <p class="h1 fw-light">Welcome to ELECTROCITY</p>
+        <p class="fs-5">Best online shop in Europe</p>
       </div>
     </div>
     <div class="carousel-item">
       <img src="../assets/images/banner2.svg" class="d-block w-100" style="object-fit:cover" alt="...">
       <div class="carousel-caption d-none d-md-block">
-        <h5>Second slide label</h5>
-        <p>Some representative placeholder content for the second slide.</p>
+        <p class="h1 fw-light">logitech x electroCity</p>
+        <p>Best devices by logitech</p>
       </div>
     </div>
     <div class="carousel-item">
-      <img src="../assets/images/banner3.svg" class="d-block w-100" style="object-fit:cover"alt="...">
+      <img src="../assets/images/banner3.svg" class="d-block w-100" style="object-fit:cover" alt="...">
       <div class="carousel-caption d-none d-md-block">
-        <h5>Third slide label</h5>
-        <p>Some representative placeholder content for the third slide.</p>
+      <p class="h1 fw-light">razer x electroCity</p>
+        <p>"For gamers by gamers." devices can find in our e-shop</p>
       </div>
     </div>
   </div>
-  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselBanner" data-bs-slide="prev">
     <span class="carousel-control-prev-icon" aria-hidden="true"></span>
     <span class="visually-hidden">Previous</span>
   </button>
-  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselBanner" data-bs-slide="next">
     <span class="carousel-control-next-icon" aria-hidden="true"></span>
     <span class="visually-hidden">Next</span>
   </button>
 </div>
 
-  <div class="d-flex justify-content-center mt-5">
-    <h3>Catalogue</h3>
-  </div>
-  <div class="catalogue row m-0 d-flex justify-content-center gap-2 mb-5 pb-2">
+<!-- CATALOGUE -->
 
-    <a href="#" class="card" style="width: 18rem;">
-      <div>
-        <img src="..." class="card-img-top" alt="...">
-        <div class="card-body">
-          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-        </div>
+<div class="d-flex justify-content-center mt-5">
+  <h3>Catalogue</h3>
+</div>
+<div class="catalogue row m-0 d-flex justify-content-center gap-2 mb-5 pb-2">
+
+  <a href="#" class="card" style="width: 18rem;">
+    <div>
+      <img src="..." class="card-img-top" alt="...">
+      <div class="card-body">
+        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
       </div>
-    </a>
-    <a href="#" class="card" style="width: 18rem;">
-      <div>
-        <img src="..." class="card-img-top" alt="...">
-        <div class="card-body">
-          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-        </div>
-      </div>
-    </a>
-    <a href="#" class="card" style="width: 18rem;">
-      <div>
-        <img src="..." class="card-img-top" alt="...">
-        <div class="card-body">
-          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-        </div>
-      </div>
-    </a>
-    <a href="#" class="card" style="width: 18rem;">
-      <div>
-        <img src="..." class="card-img-top" alt="...">
-        <div class="card-body">
-          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-        </div>
-      </div>
-    </a>
-    <a href="#" class="card" style="width: 18rem;">
-      <div>
-        <img src="..." class="card-img-top" alt="...">
-        <div class="card-body">
-          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-        </div>
-      </div>
-    </a>
-    <a href="#" class="card" style="width: 18rem;">
-      <div>
-        <img src="..." class="card-img-top" alt="...">
-        <div class="card-body">
-          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-        </div>
-      </div>
-    </a>
-    <a href="#" class="card" style="width: 18rem;">
-      <div>
-        <img src="..." class="card-img-top" alt="...">
-        <div class="card-body">
-          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-        </div>
-      </div>
-    </a>
-    <a href="#" class="card" style="width: 18rem;">
-      <div>
-        <img src="..." class="card-img-top" alt="...">
-        <div class="card-body">
-          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-        </div>
-      </div>
-    </a>
-    <a href="#" class="card" style="width: 18rem;">
-      <div>
-        <img src="..." class="card-img-top" alt="...">
-        <div class="card-body">
-          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-        </div>
-      </div>
-    </a>
-    <a href="#" class="card" style="width: 18rem;">
-      <div>
-        <img src="..." class="card-img-top" alt="...">
-        <div class="card-body">
-          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-        </div>
-      </div>
-    </a>
-    <div class="d-flex justify-content-center mt-5">
-      <a href="catalogue/" type="submit" class="btn btn-primary btn-lg mb-5 catalogue-btn">Catalogue</a>
     </div>
-
+  </a>
+  <a href="#" class="card" style="width: 18rem;">
+    <div>
+      <img src="..." class="card-img-top" alt="...">
+      <div class="card-body">
+        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+      </div>
+    </div>
+  </a>
+  <a href="#" class="card" style="width: 18rem;">
+    <div>
+      <img src="..." class="card-img-top" alt="...">
+      <div class="card-body">
+        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+      </div>
+    </div>
+  </a>
+  <a href="#" class="card" style="width: 18rem;">
+    <div>
+      <img src="..." class="card-img-top" alt="...">
+      <div class="card-body">
+        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+      </div>
+    </div>
+  </a>
+  <a href="#" class="card" style="width: 18rem;">
+    <div>
+      <img src="..." class="card-img-top" alt="...">
+      <div class="card-body">
+        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+      </div>
+    </div>
+  </a>
+  <a href="#" class="card" style="width: 18rem;">
+    <div>
+      <img src="..." class="card-img-top" alt="...">
+      <div class="card-body">
+        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+      </div>
+    </div>
+  </a>
+  <a href="#" class="card" style="width: 18rem;">
+    <div>
+      <img src="..." class="card-img-top" alt="...">
+      <div class="card-body">
+        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+      </div>
+    </div>
+  </a>
+  <a href="#" class="card" style="width: 18rem;">
+    <div>
+      <img src="..." class="card-img-top" alt="...">
+      <div class="card-body">
+        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+      </div>
+    </div>
+  </a>
+  <a href="#" class="card" style="width: 18rem;">
+    <div>
+      <img src="..." class="card-img-top" alt="...">
+      <div class="card-body">
+        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+      </div>
+    </div>
+  </a>
+  <a href="#" class="card" style="width: 18rem;">
+    <div>
+      <img src="..." class="card-img-top" alt="...">
+      <div class="card-body">
+        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+      </div>
+    </div>
+  </a>
+  <div class="d-flex justify-content-center mt-5">
+    <a href="catalogue/" type="submit" class="btn btn-primary btn-lg mb-5 catalogue-btn">Catalogue</a>
   </div>
+
+</div>
 </div>
 
 
