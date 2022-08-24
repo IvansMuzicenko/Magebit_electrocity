cdb16552311e3b3e30d560e889c806b0ef8cde17
Dmitrijs <dmitrijs.sinickis@gmail.com>
2022-08-24 14:06:17 +0300

    fix catalogue item img & add button

--- resources/views/catalogue-item.blade.php
+++ resources/views/catalogue-item.blade.php
@@ -6,13 +6,13 @@ require_once "./templates/header.php"
     <div id="productCarousel" class="carousel slide" data-bs-interval="3000" data-bs-ride="carousel">
         <div class="carousel-inner">
             <div class="carousel-item active">
-                <img src="..." class="img1 d-block" alt="...">
+                <img src="..." class="img1 d-block" alt="..." >
             </div>
             <div class="carousel-item">
-                <img src="..." class="img2 d-block" alt="...">
+                <img src="..." class="img2 d-block" alt="..." >
             </div>
             <div class="carousel-item">
-                <img src="..." class="img3 d-block" alt="...">
+                <img src="..." class="img3 d-block" alt="..." >
             </div>
         </div>
         <button class="carousel-control-prev" type="button" data-bs-target="#productCarousel" data-bs-slide="prev">
@@ -37,8 +37,8 @@ require_once "./templates/header.php"
         <li class="list-group-item">Color: <span class="color fw-bold"></span></li>
         <li class="list-group-item">Connection type: <span class="connection fw-bold"></span></li>
     </ul>
-    <div class="card-body">
-        <a href="#" class="card-link addBtn">Add</a>
+    <div class="card-body d-flex justify-content-end">
+        <a href="#" id="add" class="card-link btn btn-primary btn-lg mb-5 catalogue-btn">Add</a>
     </div>
 </div>
 
